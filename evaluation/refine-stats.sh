28e850b3ac1a5b63d185452f32eb4f4edcd2ecf9
Mike Park <Mike.Park@NASA.Gov>
2016-12-16 09:40:31 -0500

    compare old

---
+++ evaluation/refine-stats.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+meshb=$1
+metric=temp.metric
+hmin=0.001
+
+ref_acceptance ${meshb} ${metric} ${hmin}
+ref_metric_test ${meshb} ${metric} > status.txt
+
+gnuplot ref_histogram_edge-ratio.gnuplot
+epstopdf ref_histogram_edge-ratio.eps
+
+rm -f ${metric}
+rm -f ref_histogram_edge-ratio.eps
+rm -f ref_histogram_edge-ratio.gnuplot
+rm -f ref_metric_test_s00_n1_p0_ellipse.tec
+rm -f ref_metric_test_surf.tec
+
