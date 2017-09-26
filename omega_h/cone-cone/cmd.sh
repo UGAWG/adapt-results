fa09dee714bfe5c68349fac12a7f8dceb548efe4
Dan Ibanez <daibane@sandia.gov>
2017-09-26 07:49:00 -0600

    record version used

--- omega_h/cone-cone/cmd.sh
+++ omega_h/cone-cone/cmd.sh
@@ -1,1 +1,2 @@
+#using Omega_h version: [develop 1468218] have cone-cone use meshb format
 ~/build/gcc/omega_h/src/ugawg_cone_cone ~/src/adapt-benchmarks/cone-cone/cone-cone.meshb --model ~/src/adapt-benchmarks/cone-cone/cone-cone.step --viz viz cone-cone.meshb
